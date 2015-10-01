4e7613c616177d48529125596529977d5994d38b
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-10-01 12:57:27 -0400

    Initial Commit

---
+++ Coffee DeciderTests/Coffee_DeciderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Coffee_DeciderTests.swift
+//  Coffee DeciderTests
+//
+//  Created by Rosario Tarabocchia on 10/1/15.
+//  Copyright © 2015 RLDT. All rights reserved.
+//
+
+import XCTest
+@testable import Coffee_Decider
+
+class Coffee_DeciderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
